9a0d0bb49700969eb7d08d7a5d87cd497343b1a8
DeanKelly751 <62704110+DeanKelly751@users.noreply.github.com>
2021-12-08 16:52:59 +0000

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,12 +1,10 @@
 import speech_recognition as sr
 import time
 import schedule
-from func import talk
+from func import talk, take_command
 
 listener = sr.Recognizer()
 
-def remind_me(time):
+def remind_me():
+
     talk("Hi user, please  dont forgeet to check your enviromental sensr readings.")
-
-
-job = schedule.every().day.at(time).do(remind_me)
